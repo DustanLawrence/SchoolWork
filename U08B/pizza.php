8d91591d43c8c98e315f19928bb0ad2ed2d4306d
DustanLawrence <105127776+DustanLawrence@users.noreply.github.com>
2022-06-06 17:58:30 -0700

    Update pizza.php

--- U08B/pizza.php
+++ U08B/pizza.php
@@ -16,8 +16,18 @@ What objectives are you demonstrating?
 -->
 
 <style>
-
-
+h1{
+	text-align:center;
+	font-size:4.0em;
+	}
+	
+	body{
+	margin:50px 0px; padding:10px;
+    text-align:center;
+    align:center;
+	background-color: #EBA586;
+	}	
+	
 </style>
 
 </head>
@@ -29,11 +39,41 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Get data from a GET or POST (change GET to POST for post)
-if(isset($_GET['data'])) { $data = $_GET['data']; } else { $data = ""; }
+if(isset($_POST["name"])) { $name = $_POST["name"]; } else { $name = ""; }
+if(isset($_POST["size"])) { $size = $_POST["size"]; } else { $size = ""; }
+if(isset($_POST["pepp"])) { $pepp = $_POST["pepp"]; } else { $pepp = ""; }
+if(isset($_POST["mush"])) { $mush = $_POST["mush"]; } else { $mush = ""; }
+if(isset($_POST["saus"])) { $saus = $_POST["saus"]; } else { $saus = ""; }
+if(isset($_POST["oliv"])) { $oliv = $_POST["oliv"]; } else { $oliv = ""; }
+$total=0;
+$tax=0;
+if($size == 'Small'){
+	$total+=9.99;
+	}
+if($size == 'Medium'){
+	$total+=12.99;
+	}
+if($size == 'Large'){
+	$total+=15.99;
+	}
 
-print "(body of your project) $data";
 
-for($i; $i < 10; $i++) { print " $i"; }
+print "
+<h1>Pizza Orders Made Easy</h1><br />
+<h2>Reciept</h2>
+<p>Customer Name: $name </p>
+<p>Pizza Size: $size - $$total</p> 
+";
+if($mush){$total+=2.99;
+print"<p>Mushroom's-$2.99</p>";
+	}
+if($oliv){$total+=2.99;
+print"<p>Olives-$2.99</p>";}
+if($pepp){$total+=2.99;
+print"<p>Pepperonni-$2.99</p>";}
+if($saus){$total+=2.99;
+print"<p>Sausage-$2.99</p>";}
+print"<p>Total Due: $$total</p>";
 
 ?>
 </body>

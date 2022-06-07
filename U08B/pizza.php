f7618418334eb2018f860a865a555a415133ef4a
DustanLawrence <105127776+DustanLawrence@users.noreply.github.com>
2022-06-06 20:21:57 -0700

    Update pizza.php

--- U08B/pizza.php
+++ U08B/pizza.php
@@ -11,11 +11,12 @@ U08B interactive pizza ordering
 
 What objectives are you demonstrating?
 
-(place here)
+*using POST *using PHP *if statement in PHP *printing to screen with php
 
 -->
 
 <style>
+	
 h1{
 	text-align:center;
 	font-size:4.0em;
@@ -47,23 +48,27 @@ if(isset($_POST["saus"])) { $saus = $_POST["saus"]; } else { $saus = ""; }
 if(isset($_POST["oliv"])) { $oliv = $_POST["oliv"]; } else { $oliv = ""; }
 $total=0;
 $tax=0;
+	//set if size is small
 if($size == 'Small'){
 	$total+=9.99;
 	}
+	//set if size is medium
 if($size == 'Medium'){
 	$total+=12.99;
 	}
+	//set if size is large
 if($size == 'Large'){
 	$total+=15.99;
 	}
 
-
+//print top of invoice
 print "
 <h1>Pizza Orders Made Easy</h1><br />
 <h2>Reciept</h2>
 <p>Customer Name: $name </p>
 <p>Pizza Size: $size - $$total</p> 
 ";
+	//calculate invoice
 if($mush){$total+=2.99;
 print"<p>Mushroom's-$2.99</p>";
 	}
